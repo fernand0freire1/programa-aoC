1ecc4431764c2de38edf0147da1dabb8c21ff5ed
fernand0freire1 <fernandof23146@gmail.com>
2025-02-03 17:00:19 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main (){ 
+//sintaxe scanf 
+//scanf("formato1", "formato2, &variavel1 variavel 2 , ...);
+  printf ("Entre com sua dade\n");
+  scanf ("%d", &idade);
+  printf ("Idade: %d\n", idade);
+  printf ("Entre com a altura\n");
+  scanf ("%f, %altrura;);
+  printf ("Entra com a opcao\n");
+  scanf("%c,opcao);
+  printf ("%c", opcao);
 
+}
